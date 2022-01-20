9edb61ea9430572170b86e9d41c1aa18a6b911e7
산소 <83737364+CSense-O2@users.noreply.github.com>
2022-01-20 22:19:52 +0900

    v4.7.0
    ※ 아이디 변경에 따른 링크 코드 수정
    ※ 커스텀 BGM 파일 백업 및 복원 추가

--- SBC.py
+++ SBC.py
@@ -1,6 +1,6 @@
 # -*- Encoding:UTF-8 -*- #
 ### 코드를 무단으로 복제,개조 및 배포하지 말 것 ###
-### Copyright ⓒ 2021 CSense-O2 / 산소 my______baby@naver.com ###
+### Copyright ⓒ 2021-2022 CSense-O2 / 산소 my______baby@naver.com ###
 import os
 import sys
 import shutil
@@ -13,11 +13,11 @@ from string import ascii_uppercase
 from bs4 import BeautifulSoup
 from requests import get
 
-현재버전 = 'v4.6.2'
+현재버전 = 'v4.7.0'
 
 real_path = os.getcwd()
 exe_path = real_path.replace('\\', '/')
-backup_path = os.path.expanduser('~')+'/Desktop/SBC_backup/filepath.txt'
+backup_path = os.path.expanduser('~')+'/Desktop/SBC_backup'
 for drive in list(ascii_uppercase):
     for (path, dir, files) in os.walk(drive+':/'):
         if 'DNF.exe' in files:
@@ -25,14 +25,9 @@ for drive in list(ascii_uppercase):
             break
 abs_path = DNF_path+'/Music'
 
-with open(exe_path+'/MainFolder/다시보지않기.txt', 'r', encoding='utf-8') as file:
-    read = file.read()
-
-url = 'https://github.com/CSense-O2/SBC/releases/latest'
+url = 'http://bit.do/SBC-releases-latest'
 
 response = get(url)
-
-download_list = []
 
 if response.status_code == 200:
     html = response.text
@@ -46,28 +41,32 @@ if response.status_code == 200:
 """+updatelog+'\n'
     except AttributeError:
         msgbox.showinfo('최신버전 확인 오류', '최신 버전 확인에 오류가 발생했습니다.\r홈페이지를 확인해주세요.')
-        webbrowser.open('https://github.com/CSense-O2/SBC/labels/Error')
+        webbrowser.open('http://bit.doSBC-homepage')
         sys.exit(0)
     if 최신버전 > 현재버전:
         q1 = msgbox.askquestion(
             '최신 버전 발견', '최신 버전 다운로드를 위해 링크가 열립니다.\n커스텀 브금 파일을 백업하시겠습니까?')
         if q1 == 'yes':
-            shutil.copyfile(exe_path+'/MainFolder/filepath.txt', backup_path)
-        msgbox.showinfo('최신 버전 발견', '최신 버전 다운로드를 위해 링크가 열립니다.')
-        webbrowser.open('https://github.com/CSense-O2/SBC')
+            shutil.copyfile(exe_path+'/MainFolder/filepath.txt', backup_path+'/filepath.txt')
+            shutil.copytree(exe_path+'/After',backup_path+'/After')
+        else:
+            pass
+        with open(exe_path+'/MainFolder/다시보지않기.txt','w',encoding='UTF-8') as f:
+            f.write('')
+        msgbox.showinfo('최신 버전 발견','최신 버전 다운로드를 위한 링크가 열립니다.')
+        webbrowser.open('http://bit.do/SBC-homepage')
         sys.exit(0)
     elif 최신버전 <= 현재버전:
         pass
     else:
         msgbox.showerror('버전 확인 오류', '관리자에게 "버전 확인 오류"라고 전달해주세요.')
-        webbrowser.open('https://github.com/CSense-O2/SBC/labels/Error')
+        webbrowser.open('http://bit.do/SBC-homepage')
 elif response.status_code == 404:
     msgbox.showinfo('현재 점검중입니다.', '현재 점검중이니 관리자에게 문의해주세요.')
-    webbrowser.open('https://github.com/CSense-O2/SBC/labels/Error')
+    webbrowser.open('http://bit.do/SBC-homepage')
 else:
-    msgbox.showerror("파싱 오류", 'response : ' +
-                     response.status_code+"\n해당 오류 코드를 관리자에게 전달해 주세요.")
-    webbrowser.open('https://github.com/CSense-O2/SBC/labels/Error')
+    msgbox.showerror("파싱 오류", 'response : ' +response.status_code+"\n해당 오류 코드를 관리자에게 전달해 주세요.")
+    webbrowser.open('http://bit.do/SBC-homepage')
 
 root = Tk()
 root.title("SBC")
@@ -81,6 +80,9 @@ root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 root.resizable(False, False)
 root.iconbitmap(exe_path+'/MainFolder/icon.ico')
 
+with open(exe_path+'/MainFolder/다시보지않기.txt', 'r', encoding='UTF-8') as file:
+    read = file.read()
+
 if '다시보지않기' not in read:
     toplevel = Toplevel(root)
     toplevel.title('업데이트 내역')
@@ -88,29 +90,23 @@ if '다시보지않기' not in read:
     toplevel.iconbitmap(exe_path+'/MainFolder/icon.ico')
     toplevel.wm_attributes("-topmost", 1)
     Label(toplevel, text=patchnote).pack(padx=10, pady=5)
-
     def 다시보지않기():
         with open(exe_path+'/MainFolder/다시보지않기.txt', 'w', encoding='UTF-8') as file:
             file.write('다시보지않기')
         toplevel.destroy()
     Button(toplevel, text='다시보지않기', command=다시보지않기).pack(padx=10, pady=5)
 
-
 def link_btn():
-    webbrowser.open('https://github.com/CSense-O2/SBC/labels')
-
+    webbrowser.open('http://bit.do/SBC-homepage')
 
 def update_log():
     msgbox.showinfo("업데이트 내용 확인", patchnote)
 
-
 def install_btn():
     msgbox.showinfo("던파 설치 경로", DNF_path)
 
-
 def version_btn():
     msgbox.showinfo("현재 버전", '[ '+현재버전+' ]')
-
 
 def change_btn():
     toplevel = Toplevel(root)
@@ -124,51 +120,54 @@ def change_btn():
     toplevel.geometry('%dx%d+%d+%d' % (w, h, x, y))
     toplevel.iconbitmap(exe_path+'/MainFolder/icon.ico')
     def change_btn(number):
-        number_dir = filedialog.askopenfile(
-            initialdir="/", title=number+"번 관문 BGM 파일 선택", filetypes=(("OGG files", "*.ogg"), ("all files", "*.*")))
-        if filedialog.Open():
-            dir_name = number_dir.name
-            file_name = number_dir.name.split('/')[-1]
-            q2 = msgbox.askquestion(
-                number+"번 관문 BGM 설정 알림", file_name+'을 \n'+number+'번 관문의 BGM 파일로 설정하시겠습니까?')
-            if q2 == 'yes':
-                with open(exe_path+'/MainFolder/filepath.txt', 'r', encoding='UTF-8') as f:
-                    file = f.read().split('\n')
-                if number == '1':
-                    file_list = file[0].replace(file[0], dir_name)+'\n'+'\n'.join(file[1:])
-                elif number == '2':
-                    file_list = ''.join(file[0])+'\n'+file[1].replace(file[1], dir_name)+'\n'+'\n'.join(file[2:])
-                elif number == '3':
-                    file_list = '\n'.join(file[:2])+'\n'+file[2].replace(file[2], dir_name)+'\n'+''.join(file[3])
-                elif number == '4':
-                    file_list = '\n'.join(file[:3])+'\n'+file[3].replace(file[3], dir_name)
-                else:
-                    msgbox.showerror("관문 번호 로딩 오류", '관리자에게 "관문 번호 로딩 오류" 라고 전달해주세요')
-                with open(exe_path+'/MainFolder/filepath.txt', 'w', encoding='UTF-8') as file:
-                    file.write(file_list)
-                msgbox.showinfo(number+'번 관문 BGM 설정 변경 완료', number +'번 관문의 BGM 파일 설정이 '+file_name+'으로 변경되었습니다.')
+        number_dir = filedialog.askopenfile(initialdir="/", title=number+"번 관문 BGM 파일 선택", filetypes=(("OGG files", "*.ogg"), ("all files", "*.*")))
+        file_name = number_dir.name.split('/')[-1]
+        with open(exe_path+'/MainFolder/filepath.txt','r',encoding='UTF-8') as f:
+            file_path = f.read()
+        current_file_name = file_path.split('\n')[int(number)-1]
+        change_question = msgbox.askquestion(number+"번 관문 BGM 설정 알림", '현재 '+number+'번 관문의 BGM 파일은 '+current_file_name+'입니다.\n'+file_name+'으로 변경하시겠습니까?')
+        if change_question == 'yes':
+            file_path.replace(current_file_name,file_name)
+            if number == '1':
+                shutil.copyfile(file_path,exe_path+'/After/siroco_broken_d.ogg')
+            elif number == '2':
+                shutil.copyfile(file_path,exe_path+'/After/siroco_broken_o1.ogg')
+            elif number == '3':
+                shutil.copyfile(file_path,exe_path+'/After/siroco_broken_o2.ogg')
+            elif number == '4':
+                shutil.copyfile(file_path,exe_path+'/After/siroco_broken_r.ogg')
+            else:
+                msgbox.showerror('관문 번호 오류','관리자에게 "관문 번호 오류" 라고 전달해주세요.')
+                webbrowser.open('http://bit.do/SBC-homepage')
+                sys.exit(0)
+        with open(exe_path+'/MainFloder/filepath.txt','w',encoding='UTF-8') as f:
+            f.write(file_path)
+        msgbox.showinfo(number+'번 관문 BGM 설정 변경 완료', number +'번 관문의 BGM 파일 설정이 '+file_name+'으로 변경되었습니다.')
     def reset_btn(number):
         q3 = msgbox.askquestion(number+'번 관문 BMG 설정 초기화 확인',number+'번 관문의 BGM 파일 설정을 애국가로 초기화하시겠습니까?')
         if q3 == 'yes':
             with open(exe_path+'/MainFolder/filepath.txt', 'r', encoding='UTF-8') as f:
-                file = f.read().split('\n')
+                file = f.read()
+            file.replace(file.split('\n')[int(number)-1],'애국가')
+            with open(exe_path+'/MainFolder/filepath.txt', 'w', encoding='UTF-8') as f:
+                f.write(file)
             if number == '1':
-                file_list = 'siroco_broken_d.ogg\n'+'\n'.join(file[1:])
+                shutil.copyfile(exe_path+'/Before/siroco_broken_d.ogg',exe_path+'/After/siroco_broken_d.ogg')
             elif number == '2':
-                file_list = ''.join(file[0])+'\n'+'siroco_broken_o1.ogg'+'\n'+'\n'.join(file[2:])
+                shutil.copyfile(exe_path+'/Before/siroco_broken_o1.ogg',exe_path+'/After/siroco_broken_o1.ogg')
             elif number == '3':
-                file_list = '\n'.join(file[:2])+'\n'+'siroco_broken_o2.ogg\n'+''.join(file[3])
+                shutil.copyfile(exe_path+'/Before/siroco_broken_o2.ogg',exe_path+'/After/siroco_broken_o2.ogg')
             elif number == '4':
-                file_list = '\n'.join(file[:3])+'\nsiroco_broken_r.ogg'
-                with open(exe_path+'/MainFolder/filepath.txt', 'w', encoding='UTF-8') as f:
-                    f.write(file_list)
+                shutil.copyfile(exe_path+'/Before/siroco_broken_r.ogg',exe_path+'/After/siroco_broken_r.ogg')
             else:
-                msgbox.showerror("관문 번호 로딩 오류", '관리자에게 "관문 번호 로딩 오류" 라고 전달해주세요')
-                webbrowser.open('http://pf.kakao.com/_laxars/chat')
+                msgbox.showerror('관문 번호 오류','관리자에게 "관문 번호 오류" 라고 전달해주세요.')
+                webbrowser.open('http://bit.do/SBC-homepage')
+                sys.exit(0)
             msgbox.showinfo(number+'번 관문 BMG 파일 설정 초기화 완료',number+'번 관문의 BGM 파일 설정이 애국가로 초기화 되었습니다.')
     def all_change_btn():
         with open(exe_path+'/MainFolder/filepath.txt', 'w', encoding='utf-8') as file:
-            file.write('siroco_broken_d.ogg\nsiroco_broken_o1.ogg\nsiroco_broken_o2.ogg\nsiroco_broken_r.ogg')
+            file.write('애국가\n애국가\n애국가\n애국가')
+        shutil.copytree(exe_path+'/Before',exe_path+'/After')
         msgbox.showinfo('모든 관문 BGM 설정 초기화 완료', '모든 관문의 BGM 설정이 초기화 되었습니다.')
     Label(toplevel, text='   ').grid(row=0, column=0)
     Button(toplevel, bg='White', width=10, height=2, text='모두 초기화', command=all_change_btn).grid(row=1, column=2)
@@ -180,6 +179,11 @@ def change_btn():
     Button(toplevel, bg='White', width=10, height=2, text='2번 초기화', command=lambda: reset_btn('2')).grid(row=3, column=3)
     Button(toplevel, bg='White', width=10, height=2, text='3번 초기화', command=lambda: reset_btn('3')).grid(row=4, column=3)
     Button(toplevel, bg='White', width=10, height=2, text='4번 초기화', command=lambda: reset_btn('4')).grid(row=5, column=3)
+
+def check_btn():
+    with open(exe_path+'/MainFolder/filepath.txt','r',encoding='UTF-8') as f:
+        file = f.read()
+    msgbox.showinfo('BGM 파일 설정 확인','현재 설정된 BGM 파일은 다음과 같습니다.\n1번 관문 : '+file.split('\n')[0]+'\n2번 관문 : '+file.split('\n')[1]+'\n3번 관문 : '+file.split('\n')[2]+'\n4번 관문 : '+file.split('\n')[3])
 
 def up_btn():
     toplevel = Toplevel(root)
@@ -218,17 +222,25 @@ def transparency_btn():
     slide_bar.pack(pady=5)
 
 def restore_btn():
-    if os.path.isfile(backup_path):
-        q4 = msgbox.askquestion('파일 경로 복원', '브금 파일들의 경로를 복원하시겠습니까?')
+    if os.path.isdir(backup_path):
+        q4 = msgbox.askquestion('파일 경로 복원', '브금 파일들을 복원하시겠습니까?')
         if q4 == 'yes':
-            shutil.copyfile(backup_path, exe_path+'/MainFolder/filepath.txt')
+            shutil.copyfile(backup_path+'/filepath.txt', exe_path+'/MainFolder/filepath.txt')
+            shutil.copytree(backup_path+'/After',exe_path+'/After')
+    else:
+        msgbox.showerror('파일 복원 실패','백업 파일들이 존재하지 않습니다')
 
 def backup_btn():
-    if os.path.isfile(backup_path):
+    if os.path.isdir(backup_path):
         q5 = msgbox.askquestion(
             '파일 복원 확인', '해당 경로에 복원파일이 이미 존재합니다.\n파일을 덮어쓰시겠습니까?')
         if q5 == 'yes':
-            shutil.copyfile(exe_path+'/MainFolder/filepath.txt', backup_path)
+            shutil.copyfile(exe_path+'/MainFolder/filepath.txt', backup_path+'/filepath.txt')
+            shutil.copytree(exe_path+'/After',backup_path+'/After')
+    else:
+        os.mkdir(backup_path)
+        shutil.copyfile(exe_path+'/MainFolder/filepath.txt', backup_path+'/filepath.txt')
+        shutil.copytree(exe_path+'/After',backup_path+'/After')
 
 def read_all_file(path):
     output = os.listdir(path)
@@ -266,11 +278,11 @@ def apply_btn_cmd():
                 shutil.copyfile(exe_path+'/After/'+file,abs_path+'/'+original[num])
             else:
                 msgbox.showinfo('파일 복사 오류', '관리자에게 "파일 복사 오류"라고 전달해주세요.')
-                webbrowser.open('https://github.com/CSense-O2/SBC/labels/Error')
+                webbrowser.open('http://bit.do/SBC-homepage')
         msgbox.showinfo("알림", "관문 BGM 파일 적용 완료")
     elif 'error' in exist:
         msgbox.showerror('적용하기 오류', '관리자에게 "적용하기 오류"라고 전달해주세요.')
-        webbrowser.open('https://github.com/CSense-O2/SBC/labels/Error')
+        webbrowser.open('http://bit.do/SBC-homepage')
     else:
         msgbox.showwarning('던파 미실행', '던파 미실행 상태에서 적용시 적용이 불가능합니다.')
 
@@ -283,6 +295,7 @@ menu = Menu(root)
 menu_update = Menu(menu, tearoff=0, relief='groove')
 menu_update.add_command(label="던파 설치 경로 확인", command=install_btn)
 menu_update.add_command(label="관문 BGM 설정 변경", command=change_btn)
+menu_update.add_command(label="관문 BGM 확인", command=check_btn)
 menu_update.add_separator()
 menu_update.add_command(label="현재 버전 확인", command=version_btn)
 menu_update.add_command(label="업데이트 로그", command=update_log)
