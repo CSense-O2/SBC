61b9d3a227715233b814a51b7279ccf2c31bf7f7
산소 <83737364+CSense-O2@users.noreply.github.com>
2022-02-13 11:53:05 +0900

    email update
    email update

--- SBC_main.py
+++ SBC_main.py
@@ -1,6 +1,6 @@
 # -*- Encoding:UTF-8 -*- #
 ### 코드를 무단으로 복제,개조 및 배포하지 말 것 ###
-### Copyright ⓒ 2021-2022 c-closed / 감는 my______baby@naver.com ###
+### Copyright ⓒ 2021-2022 c-closed / 감는 c-closed@naver.com ###
 import SBC_update
 import SBC_GUI
 
