1dcbd52d9ed5ad945db14187db702e3d1007c16f
산소 <83737364+CSense-O2@users.noreply.github.com>
2022-02-14 11:33:57 +0900

    add info
    add info

---
+++ SBC_info.py
@@ -0,0 +1,7 @@
+# -*- Encoding:UTF-8 -*- #
+### 코드를 무단으로 복제,개조 및 배포하지 말 것 ###
+### Copyright ⓒ 2021-2022 c-closed / 감는 c-closed@naver.com ###
+import os
+from string import ascii_uppercase
+
+current_version = 'v4.7.3'
